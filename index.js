970eef6d36a8d45c4a6758b15608f81bce486596
Thamarai <tmanalan710@gmail.com>
2024-12-11 07:28:22 +0530

    Completes express.json()

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 let totalCount = 0;
 
 function middleWare(req,res,next){
     totalCount+=1;
     console.log(`The total num of Requests ${totalCount}`)
     console.log(`The Method was ${req.method}`)
-    console.log(`The time the request was hitted is ${Date.now()}`)
+    console.log(`The time the request was hitted is ${new Date()}`)
     console.log(`The Url is ${req.url}`)
     next();
 }
 app.use(middleWare)
+
+app.post("/sum",function(req, res){
+    console.log(req.body)
+
+    const a = req.body.a
+    const b = req.body.b
+
+    res.json({
+        ans: a + b
+    })
+})
 app.get("/add", function(req, res){
     const a = req.query.a
     const b = req.query.b
