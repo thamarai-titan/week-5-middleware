444d7e8ee4ed424517a6ce9e5fd287287f194889
Thamarai <tmanalan710@gmail.com>
2024-12-11 06:49:00 +0530

    Practising the middleware

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express")
 
 const app = express()
+let totalCount = 0;
+
+function middleWare(req,res,next){
+    totalCount+=1;
+    console.log(`The total num of Requests ${totalCount}`)
+    console.log(`The Method was ${req.method}`)
+    console.log(`The time the request was hitted is ${Date.now()}`)
+    console.log(`The Url is ${req.url}`)
+    next();
+}
+app.use(middleWare)
 app.get("/add", function(req, res){
     const a = req.query.a
     const b = req.query.b
